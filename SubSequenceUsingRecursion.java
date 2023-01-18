b13ca07938521d8fae6f8bff54e8a4022630f3b9
Scissorh <117175726+Scissorh@users.noreply.github.com>
2023-01-18 09:14:42 +0530

    ok

---
+++ SubSequenceUsingRecursion.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main
+{
+    static ArrayList<String> getSSQ(String s)
+    {
+        ArrayList<String> ans = new ArrayList<>();
+        if(s.length() == 0)
+        {
+            ans.add("");
+            return ans;
+        }
+        char curr=s.charAt(0);
+        ArrayList<String> smallAns = getSSQ(s.substring(1));
+        
+        for(String ss: smallAns)
+        {
+            ans.add(ss);
+            ans.add(curr +ss);
+        }
+        return ans;
+    }
+	public static void main(String[] args) {
+		ArrayList<String> ans = getSSQ("abc");
+		for(String ss : ans)
+		{
+		    System.out.println(ss);
+		}
+	}
+}
